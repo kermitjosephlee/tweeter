3d1ef9f01d37dcea044f7017da1849e4971b2689
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-07-18 11:52:16 -0400

    cleaned up and tested app.js, still need to add in render function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,7 @@
  */
 
 const tweetData = {
-  "user": {
+  user: {
     "name": "Newton",
     "avatars": {
       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
@@ -29,11 +29,38 @@ function createTweetElement (dataObj) {
   const userHandle =        dataObj.user.handle;
   const content =           dataObj.content.text;
   const createdAt =         dataObj.created_at;
+  let   currentTime =       Date.now();
+  const timeDelta =         calculateTimeSince(currentTime, createdAt);
 
+  function calculateTimeSince (now, then){
+    let differenceInTimeInMilliSec = now - then; // in milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+    const differenceInTime = Math.round(differenceInTimeInMilliSec/oneDay)
 
+    return differenceInTime;
+  };
+
+  const $postedTweet =
+    `<article class="posted-tweet">
+      <section>
+        <header>
+          <img src=${userAvatarSmall}>
+          ${userName}
+          <p class="userHandle">
+          ${userHandle}
+          </p>
+        </header>
+        <div>
+          ${content}
+        </div>
+        <footer>
+          ${timeDelta} days ago
+        </footer>
+      </section>
+    </article>`
+
+  $('#tweets-container').append($postedTweet)
 
 };
 
-var $tweet = createTweetElement (tweetData)
-
-console.log($tweet)
+createTweetElement(tweetData);
---
+++ public/scripts/saiDemo.js
@@ -0,0 +1,34 @@
+var saiData =
+
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+
+// _________________________________________________________________________________
+
+
+function createTweetElement (userObj){
+
+var name = saiData.user.name;
+var avatarSmall = saiData.user.avatars.small;
+var avatarRegular = saiData.user.avatars.regular;
+
+
+
+
+
+
+};
