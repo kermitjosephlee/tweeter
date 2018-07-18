4edeb3babf9d20add404faa1cd4f820517feaf93
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-07-18 18:18:20 -0400

    deleted hard coded tweets and site still works - validated that app.js is getting data from initial-tweets.json

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,5 @@
 $( document ).ready(function() {
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ]
 
   //*******************************************************************
 
@@ -99,7 +53,6 @@ $( document ).ready(function() {
     }
   }
 
-
 // listening for submit button click
 
 $("input").click(function(event){
@@ -107,26 +60,11 @@ $("input").click(function(event){
     const maxCharaterCount = 140;
     if (characterCount === 0 || characterCount > maxCharaterCount){
        event.preventDefault();
-       console.log("NOPE!");
+       alert("NOPE!");
     } else {
       console.log($('#textBox').val());
     }
 });
-
-
-
-
-
-
-  // function formValidator (){
-
-  //   const tempStr = $("form").serialize();
-
-  //   $.post("/tweets", tempStr, function(data, status){
-  //       $
-  //   })
-
-  // }
 
   function loadTweets() {
     $.ajax("/tweets", { method: "GET" })
@@ -139,16 +77,6 @@ $("input").click(function(event){
       });
   }
 
-  // Form Validation Logic
-  // $('#textBox').on('input', function(){
-  //   var textAreaLength = $('#textBox').val().length;
-  //   var maxCharacters = 140;
-
-  //   if (textAreaLength === null || textAreaLength > maxCharacters){
-  //     $("input").click(function(event){
-  //           event.preventDefault();
-  //   })};
-  // });
 //*******************************************************************
   loadTweets();
 
